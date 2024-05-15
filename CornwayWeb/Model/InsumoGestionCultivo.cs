827dcdd7c8d1b11e7d3ef6b96c8b5a3b40657e59
Andres Ojeda <andyojeda2003@gmail.com>
2024-05-14 21:57:38 -0500

    entidades

--- CornwayWeb/Model/InsumoGestionCultivo.cs
+++ CornwayWeb/Model/InsumoGestionCultivo.cs
@@ -1,6 +1,19 @@
-﻿namespace CornwayWeb.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CornwayWeb.Model
 {
     public class InsumoGestionCultivo
     {
+        [Key]
+        public int IdInsumoGestionCultivo { get; set; }
+        public required int IdGestionCultivo { get; set; }
+        public required int IdTipoInsumoGestionCultivo { get; set; }
+        public required string Nombre { get; set; }
+        public required string Dosis { get; set; }
+        public required string Unidad { get; set; }
+
+        public bool Activo { get; set; }
+        public virtual GestionCultivo? GestionCultivo { get; set; }
+        public virtual TipoInsumoGestionCultivo? TipoInsumoGestionCultivo { get; set; }
     }
 }
