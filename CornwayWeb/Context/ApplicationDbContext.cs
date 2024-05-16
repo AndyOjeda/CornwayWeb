75cfc6f7f8daea95ecef029a3870b384e55ba046
Andres Ojeda <andyojeda2003@gmail.com>
2024-05-15 23:19:06 -0500

    Ultimos cambios

---
+++ CornwayWeb/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CornwayWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CornwayWeb.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<TipoGestionCultivo> TipoGestionCultivos { get; set; }
+        public DbSet<TipoInsumoGestionCultivo> TipoInsumoGestionCultivos { get; set; }
+        public DbSet<TipoCultivo> TipoCultivos { get; set; }
+    }
+}
